b32ea7c97978df223b7c0cfcb033168f50967c59
Reem Yasser <76207497+ReemYasser2@users.noreply.github.com>
2022-11-17 15:54:08 +0200

    Create FactoryManagement.h

---
+++ FactoryManagement.h
@@ -0,0 +1,16 @@
+#ifndef FACTORYMANAGEMENT_H_INCLUDED
+#define FACTORYMANAGEMENT_H_INCLUDED
+
+// Declare some constants
+
+// declare to all factory management functions
+
+
+void add_student();
+void find_worker_from_product_id();
+void find_worker_from_fname();
+void tot_workers();
+void del_workers();
+void update_workers();
+
+#endif // FACTORYMANAGEMENT_H_INCLUDED
